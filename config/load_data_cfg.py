238ee8eac56449e9c5d116fa3cbdc8c4c62adef8
YiChao Huang <daniel851218@gmail.com>
2023-01-29 20:23:17 +0800

    load data configure

---
+++ config/load_data_cfg.py
@@ -0,0 +1,15 @@
+import os
+
+class Load_Data_Config():
+    shift_txt_file_train = os.path.join("dataset", "SHIFT", "train_shift.txt")
+    shift_txt_file_val = os.path.join("dataset", "SHIFT", "val_shift.txt")
+
+    bdd_txt_file_train = os.path.join("dataset", "BDD100K", "train_bdd_all.txt")
+    bdd_txt_file_val = None
+
+    driving_video_txt_file = os.path.join("dataset", "Driving_Video", "train_driving_video.txt")
+
+    img_w = 640
+    img_h = 416
+
+data_cfg = Load_Data_Config()
