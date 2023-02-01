0462a50c6a615a4c2f9bec942df005454875948f
YiChao Huang <daniel851218@gmail.com>
2023-02-01 18:27:57 +0800

    add detection head

--- detector/faster_rcnn.py
+++ detector/faster_rcnn.py
@@ -6,6 +6,7 @@ from config.train_cfg import cfg
 from common_module.resnet import ResNet50_FPN
 from common_module.rpn import RPN
 from common_module.roi import RoI_Align
+from common_module.detect_head import Detect_Head
 
 class Faster_RCNN(nn.Module):
     def __init__(self):
@@ -13,8 +14,7 @@ class Faster_RCNN(nn.Module):
         self.backbone = ResNet50_FPN()
         self.rpn = RPN(self.backbone.out_channels, len(cfg.aspect_ratio))
         self.roi_align = RoI_Align()
-        self.cls_head = None
-        self.reg_head = None
+        self.detect_head = Detect_Head(feature_channels=self.backbone.out_channels)
 
     def forward(self, imgs, targets):
         features = self.backbone(imgs)
@@ -33,9 +33,10 @@ class Faster_RCNN(nn.Module):
         # rpn_losses: dict
         #             len: batch_size
         #             keys: "loss_rpn_score", "loss_rpn_box_reg"
-        
-        instance_features, labels, reg_targets = self.roi_align(features, proposals, [(cfg.img_h, cfg.img_w)]*len(proposals), targets)
-        # instance_features: torch.Tensor, shape = (box_batch_size_per_img*batch_size, 512, 7, 7)
+
+        instance_features, labels, reg_targets, proposals = self.roi_align(features, proposals, [(cfg.img_h, cfg.img_w)]*len(proposals), targets)
+        # instance_features: torch.Tensor, shape = (box_batch_size_per_img*batch_size, backbone.out_channels, roi_align_out_size)
+        #                                        = (box_batch_size_per_img*batch_size, 512, 14, 14)
         #
         # labels: list
         #         len: batch_size
@@ -45,8 +46,16 @@ class Faster_RCNN(nn.Module):
         #              len: batch_size
         #              shape of each element: (box_batch_size_per_img, 4) = (512, 4)
         
+        embedded_ins_features,cls_scores, bbox_reg, roi_losses, detection_result = self.detect_head(instance_features, labels, reg_targets, proposals)
         
+        losses = {}
+        losses.update(rpn_losses)
+        losses.update(roi_losses)
         
-        
-        
-        return
+        return self.eager_outputs(losses, detection_result)
+    
+    @torch.jit.unused
+    def eager_outputs(self, losses, detections):
+        if self.training:
+            return losses
+        return detections
