8dcaaf8af43bfa08383616b1608894f143facb59
YiChao Huang <daniel851218@gmail.com>
2023-02-01 18:18:56 +0800

    roi parameters

--- config/train_cfg.py
+++ config/train_cfg.py
@@ -3,6 +3,12 @@ import os
 class Config():
     device = "cpu"
 
+
+
+
+
+    num_classes = 6 + 1       # foreground + background
+
     # image size
     img_w = 640
     img_h = 416
@@ -19,5 +25,17 @@ class Config():
     bg_iou_thresh = 0.3
     batch_size_per_img = 256
     positive_fraction = 0.5
+
+    # RoI parameters
+    box_fg_iou_thresh = 0.5
+    box_bg_iou_thresh = 0.5
+    box_batch_size_per_img = 512
+    box_positive_fraction = 0.25
+    box_score_thresh = 0.05
+    box_nms_thresh = 0.5
+    box_detections_per_img = 100
+
+    roi_align_out_size = [14, 14]
+    roi_sample_ratio = 2
 
 cfg = Config()
