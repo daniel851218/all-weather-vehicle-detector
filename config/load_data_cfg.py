aa37e83caf9565631d751d1effb8c9316f5374f6
YiChao Huang <daniel851218@gmail.com>
2023-02-02 13:42:46 +0800

    val_bdd

--- config/load_data_cfg.py
+++ config/load_data_cfg.py
@@ -4,8 +4,8 @@ class Load_Data_Config():
     shift_txt_file_train = os.path.join("dataset", "SHIFT", "train_shift.txt")
     shift_txt_file_val = os.path.join("dataset", "SHIFT", "val_shift.txt")
 
-    bdd_txt_file_train = os.path.join("dataset", "BDD100K", "train_bdd_all.txt")
-    bdd_txt_file_val = None
+    bdd_txt_file_train = os.path.join("dataset", "BDD100K", "train_bdd.txt")
+    bdd_txt_file_val = os.path.join("dataset", "BDD100K", "val_bdd.txt")
 
     driving_video_txt_file = os.path.join("dataset", "Driving_Video", "train_driving_video.txt")
 
