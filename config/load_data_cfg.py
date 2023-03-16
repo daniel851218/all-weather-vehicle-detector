005daf796cce601660ffd289880f589a0991bdc9
YiChao Huang <daniel851218@gmail.com>
2023-03-16 16:19:19 +0800

    test with different image type

--- config/load_data_cfg.py
+++ config/load_data_cfg.py
@@ -1,33 +1,33 @@
 import os
-from config.test_cfg import cfg
+from config.train_cfg import cfg
 
 class Load_Data_Config():
     shift_txt_file_train = os.path.join("dataset", "SHIFT", "train_shift.txt")
 
-    if cfg.img_type == "all":
+    if cfg.test_img_type == "all":
         shift_txt_file_val = os.path.join("dataset", "SHIFT", "val_shift.txt")
-    elif cfg.img_type == "daytime_normal":
+    elif cfg.test_img_type == "daytime_normal":
         shift_txt_file_val = os.path.join("dataset", "SHIFT", "val_shift_daytime_normal.txt")
-    elif cfg.img_type == "daytime_rainy":
+    elif cfg.test_img_type == "daytime_rainy":
         shift_txt_file_val = os.path.join("dataset", "SHIFT", "val_shift_daytime_rainy.txt")
-    elif cfg.img_type == "night_normal":
+    elif cfg.test_img_type == "night_normal":
         shift_txt_file_val = os.path.join("dataset", "SHIFT", "val_shift_night_normal.txt")
-    elif cfg.img_type == "night_rainy":
+    elif cfg.test_img_type == "night_rainy":
         shift_txt_file_val = os.path.join("dataset", "SHIFT", "val_shift_night_rainy.txt")
     else:
         raise ValueError("image type is not correct !")
 
     bdd_txt_file_train = os.path.join("dataset", "BDD100K", "train_bdd.txt")
 
-    if cfg.img_type == "all":
+    if cfg.test_img_type == "all":
         bdd_txt_file_val = os.path.join("dataset", "BDD100K", "val_bdd.txt")
-    elif cfg.img_type == "daytime_normal":
+    elif cfg.test_img_type == "daytime_normal":
         bdd_txt_file_val = os.path.join("dataset", "BDD100K", "val_bdd_daytime_normal.txt")
-    elif cfg.img_type == "daytime_rainy":
+    elif cfg.test_img_type == "daytime_rainy":
         bdd_txt_file_val = os.path.join("dataset", "BDD100K", "val_bdd_daytime_rainy.txt")
-    elif cfg.img_type == "night_normal":
+    elif cfg.test_img_type == "night_normal":
         bdd_txt_file_val = os.path.join("dataset", "BDD100K", "val_bdd_night_normal.txt")
-    elif cfg.img_type == "night_rainy":
+    elif cfg.test_img_type == "night_rainy":
         bdd_txt_file_val = os.path.join("dataset", "BDD100K", "val_bdd_night_rainy.txt")
     else:
         raise ValueError("image type is not correct !")
