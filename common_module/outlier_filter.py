75117f819fab2cd4efeb25b9394c555472af4504
YiChao Huang <daniel851218@gmail.com>
2023-07-17 18:05:15 +0800

    add Q3

--- common_module/outlier_filter.py
+++ common_module/outlier_filter.py
@@ -1,11 +1,12 @@
 import torch
+from config.train_test_cfg import cfg
 
 obj_filter_thresh = [
     # Car
     {
         "width": 24.0,
         "height": 20.0,
-        "ratio": 1.0,
+        "ratio": [1.0, 1.638],
         "area": 483.0
     },
 
@@ -13,7 +14,7 @@ obj_filter_thresh = [
     {
         "width": 39.0,
         "height": 29.0,
-        "ratio": 0.9666666666666668,
+        "ratio": [0.9666666666666668, 1.85],
         "area": 1110.0
     },
 
@@ -21,7 +22,7 @@ obj_filter_thresh = [
     {
         "width": 12.0,
         "height": 31.0,
-        "ratio": 0.3333333333333333,
+        "ratio": [0.3333, 0.4783],
         "area": 364.0
     },
 
@@ -29,7 +30,7 @@ obj_filter_thresh = [
     {
         "width": 40.0,
         "height": 29.0,
-        "ratio": 0.984126984126984,
+        "ratio": [0.9841, 1.9231],
         "area": 1113.0
     },
 
@@ -37,7 +38,7 @@ obj_filter_thresh = [
     {
         "width": 20.0,
         "height": 32.0,
-        "ratio": 0.4545454545454545,
+        "ratio": [0.4545, 0.9574],
         "area": 648.0
     },
 
@@ -45,7 +46,7 @@ obj_filter_thresh = [
     {
         "width": 20.0,
         "height": 27.0,
-        "ratio": 0.5060240963855421,
+        "ratio": [0.5060, 1.1471],
         "area": 528.0
     },
 ]
@@ -59,6 +60,8 @@ def filter_outlier(boxes, labels, scores, restored_ratio, restored_delta):
     filtered_scores = []
 
     for i in range(len(obj_filter_thresh)):
+        score = scores[labels == (i+1)]
+
         x1 = boxes[labels == (i+1)][:, 0]
         y1 = boxes[labels == (i+1)][:, 1]
         x2 = boxes[labels == (i+1)][:, 2]
@@ -76,16 +79,23 @@ def filter_outlier(boxes, labels, scores, restored_ratio, restored_delta):
 
         width_thresh = obj_filter_thresh[i]["width"]
         height_thresh = obj_filter_thresh[i]["height"]
-        ratio_thresh = obj_filter_thresh[i]["ratio"]
+        ratio_thresh_Q1 = obj_filter_thresh[i]["ratio"][0]
+        ratio_thresh_Q3 = obj_filter_thresh[i]["ratio"][0]
         area_thresh = obj_filter_thresh[i]["area"]
 
         width_mask = width < width_thresh
         height_mask = height < height_thresh
-        ratio_mask = ratio < ratio_thresh
+        
+        ratio_mask_Q1 = ratio < ratio_thresh_Q1
+        ratio_mask_Q3 = ratio > ratio_thresh_Q3
+        ratio_mask = torch.logical_or(ratio_mask_Q1, ratio_mask_Q3)
+        
         area_mask = area < area_thresh
 
-        mask = width_mask.to(torch.int) + height_mask.to(torch.int) + ratio_mask.to(torch.int) + area_mask.to(torch.int)
-        mask = torch.logical_not(mask > 2)
+        score_mask = score > cfg.test_strong_conf_thresh
+
+        mask = width_mask.to(torch.int) + height_mask.to(torch.int) + 1.5 * ratio_mask.to(torch.int) + 1.5 *  area_mask.to(torch.int) - 10 * score_mask
+        mask = torch.logical_not(mask >= 3)
 
         filtered_boxes += boxes[labels == (i+1)][mask].to(torch.int).tolist()
         filtered_labels += labels[labels == (i+1)][mask].tolist()
